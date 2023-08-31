9f355212fd38bc47521edd64ae21c90672431611
skanderkaroui <skander.karoui@gmail.com>
2023-08-31 18:05:35 +0100

    initialising the yolo tracking algorithm

---
+++ yolov8_tracking.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# Path of the yolov8n.pt file
+model_path = "weights/yolov8n.pt"
+
+# path of the video file
+vid_path = "videos/video_2.mp4"
+
+# Load Pre-trained ML Model
+model = YOLO(model_path)
+
+results = model.track(vid_path,
+                      conf=0.3,
+                      iou=0.5,
+                      persist=True,
+                      show=True
+                      )
+
